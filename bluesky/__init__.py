3421b3cc5bb2bd84f7cb7bddc8f80e8b4a6f7075
Philip Nye <philipnye@users.noreply.github.com>
2025-03-16 18:01:33 +0000

    Add missing type hint

--- bluesky/__init__.py
+++ bluesky/__init__.py
@@ -9,12 +9,10 @@ INPUT_DIR = "data"
 INPUT_FILENAME = "thinktanks_list.csv"
 
 
-def fetch_bluesky_data():
+def fetch_bluesky_data() -> None:
     """Download Bluesky data for list of handles"""
 
     client = authenticate()
     input_path = os.path.join(INPUT_DIR, INPUT_FILENAME)
     df_thinktanks = pd.read_csv(input_path)
     download_bluesky_data(client, df_thinktanks)
-
-    return None
