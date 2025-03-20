0c91e1283846132365558cbbcc90b0ee3b00e074
Philip Nye <philipnye@users.noreply.github.com>
2025-03-20 16:26:11 +0000

    Refactor to use Class

--- bluesky/__init__.py
+++ bluesky/__init__.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 from .client import authenticate
-from .fetch import download_bluesky_data
+from .fetch import BlueskyFetcher
 
 INPUT_DIR = "data"
 INPUT_FILENAME = "thinktanks_list.csv"
@@ -11,8 +11,8 @@ INPUT_FILENAME = "thinktanks_list.csv"
 
 def fetch_bluesky_data() -> None:
     """Download Bluesky data for list of handles"""
-
     client = authenticate()
+    fetcher = BlueskyFetcher(client)
     input_path = os.path.join(INPUT_DIR, INPUT_FILENAME)
     df_thinktanks = pd.read_csv(input_path)
-    download_bluesky_data(client, df_thinktanks)
+    fetcher.download_bluesky_data(df_thinktanks)
--- bluesky/fetch.py
+++ bluesky/fetch.py
@@ -8,48 +8,51 @@ OUTPUT_DIR = "data"
 OUTPUT_FILENAME = "thinktanks_bluesky_data.csv"
 
 
-def get_bluesky_profile(client: Client, handle: str) -> ProfileViewDetailed:
-    """Get data from Bluesky API for a given handle"""
-    data = client.get_profile(actor=handle)
+class BlueskyFetcher:
+    """Handles fetching of data from Bluesky API"""
 
-    return {
-        "did": data.did,
-        "display_name": data.display_name,
-        "created_at": data.created_at,
-        "posts_count": data.posts_count,
-        "followers_count": data.followers_count,
-    }
+    def __init__(self, client: Client):
+        self.client = client
+        self.output_dir = OUTPUT_DIR
+        self.output_filename = OUTPUT_FILENAME
 
+    def get_bluesky_profile(self, handle: str) -> ProfileViewDetailed:
+        """Get data from Bluesky API for a given handle"""
+        data = self.client.get_profile(actor=handle)
 
-def download_bluesky_data(
-    client: Client,
-    df: pd.DataFrame,
-) -> pd.DataFrame:
-    """Retrieve Bluesky data for rows of a df and save to CSV"""
+        return {
+            "did": data.did,
+            "display_name": data.display_name,
+            "created_at": data.created_at,
+            "posts_count": data.posts_count,
+            "followers_count": data.followers_count,
+        }
 
-    df[
-        [
-            "did",
-            "display_name",
-            "created_at",
-            "posts_count",
-            "followers_count",
-        ]
-    ] = df.apply(
-        lambda x: get_bluesky_profile(client, x["handle"]),
-        axis="columns",
-        result_type="expand",
-    )
+    def download_bluesky_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Retrieve Bluesky data for rows of a df and save to CSV"""
+        df[
+            [
+                "did",
+                "display_name",
+                "created_at",
+                "posts_count",
+                "followers_count",
+            ]
+        ] = df.apply(
+            lambda x: self.get_bluesky_profile(x["handle"]),
+            axis="columns",
+            result_type="expand",
+        )
 
-    # Add current date
-    df["date"] = pd.Timestamp.now().date()
+        # Add current date
+        df["date"] = pd.Timestamp.now().date()
 
-    # Save to CSV
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-    output_path = os.path.join(OUTPUT_DIR, OUTPUT_FILENAME)
-    df.to_csv(
-        output_path,
-        index=False,
-        mode="a",
-    )
-    print(f"Data saved at {output_path}")
+        # Save to CSV
+        os.makedirs(self.output_dir, exist_ok=True)
+        output_path = os.path.join(self.output_dir, self.output_filename)
+        df.to_csv(
+            output_path,
+            index=False,
+            mode="a",
+        )
+        print(f"Data saved at {output_path}")
